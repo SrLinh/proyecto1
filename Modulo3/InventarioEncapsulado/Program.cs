f87df9eed7c5d2e8d3d1ab38b473736888c311eb
MarioLinares <mariostevjimenez@gmail.com>
2026-05-19 15:51:38 -0600

    Encapsulamiento

---
+++ Modulo3/InventarioEncapsulado/Program.cs
@@ -0,0 +1,39 @@
+﻿class Producto
+{
+    public string nombre {get; private set;}
+    public double precio {get; private set;}
+    public int stock {get; private set;}
+
+    public Producto(string Nombre, double Precio, int Stock)
+    {
+        nombre = Nombre;
+        precio = Precio;
+        stock = Stock;
+    }
+    public void Vender()
+    {
+        if (true)
+        {
+            
+        }
+    }
+    public void AumentarStock()
+    {
+        
+    }
+
+    public void MostarInfo()
+    {
+
+        Console.WriteLine($"{nombre} || {precio} || {stock}");
+    }
+}
+class Program
+{
+    static void Main (string [] args)
+    {
+        Producto p1 = new Producto("Cosa", 2.5, 2);
+        p1.MostarInfo();
+    }
+}
+
---
+++ Modulo3/registroEstudiantes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection.Metadata;
+class Estudiante
+{
+    public string nombre {get; set;}
+    public int edad {get; set;}
+    public double notaFinal {get; set;}
+
+    public Estudiante(string Nombre, int Edad, double NotaFinal)
+    {
+        if (string.IsNullOrWhiteSpace(Nombre))
+        {
+            Console.WriteLine("El dato esta vacio");
+        }
+        else
+        {
+            nombre = Nombre;
+        }
+        if (Edad < 5 || Edad > 100)
+        {
+            Console.WriteLine("La edad tiene que ser mayor a 5 años y menor a 100 años");
+        }
+        else
+        {
+            edad = Edad;
+        }
+
+
+        if (NotaFinal < 0 || NotaFinal > 10)
+        {
+            Console.WriteLine("La nota final tiene que estar entre 0 y 10");
+        }
+        else
+        {
+            notaFinal = NotaFinal;
+        }
+    }
+    public void CambiarNota(double nuevaNota)
+    {
+        if (nuevaNota < 0 || nuevaNota > 10 )
+        {
+            Console.WriteLine("La nueva nota final tiene que estar entre 0 y 10");
+        }else
+        {
+            notaFinal = nuevaNota;
+        }
+    }
+    public void MostrarResumen()
+    {
+        
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Estudiante e1 = new Estudiante("Mario", 14, 9);
+        Estudiante e2 = new Estudiante("Gepeto", 3, -1);
+
+
+    }
+}
