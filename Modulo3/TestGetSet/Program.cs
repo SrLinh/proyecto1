af3cd607db5c41cceef8f8d5a370310ad1921b77
MarioLinares <mariostevjimenez@gmail.com>
2026-05-29 11:59:33 -0600

    se agrego un test sobre get y set

---
+++ Modulo3/TestGetSet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Producto
+{
+    private double precio;
+
+    public double Precio
+    {
+        get
+        {
+            return precio;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                precio = value;
+            }
+            else
+            {
+                Console.WriteLine("El precio no puede ser negativo");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Producto p = new Producto();
+
+        p.Precio = 50;   // SET válido
+        Console.WriteLine(p.Precio); // GET
+
+        p.Precio = -10;  // SET inválido
+    }
+}
