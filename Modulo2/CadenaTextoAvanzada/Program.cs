03ee1a7a5fddecfc6402b2f518ba7c58e0e9d87e
SrLinh <mariostevjimenez@gmail.com>
2026-06-10 19:32:42 -0600

    Commit

--- Modulo2/CadenaTextoAvanzada/Program.cs
+++ Modulo2/CadenaTextoAvanzada/Program.cs
@@ -16,13 +16,52 @@ class Program
     static void Main (string [] args)
     {
         Console.WriteLine("Ingresa texto 1");
-        string text1 = Console.ReadLine();
+        string texto = Console.ReadLine();
         Console.WriteLine("ingresa texto 2");
-        string text2 = Console.ReadLine();
+        string texto2 = Console.ReadLine();
 
-        bool comparacionTexto = text1.Equals(text2, StringComparison.OrdinalIgnoreCase);
+        bool comparacionTexto = texto.Equals(texto2, StringComparison.OrdinalIgnoreCase);
         Console.WriteLine($"El texto 1 y 2 son iguales? {comparacionTexto}");
 
+        Console.WriteLine("------- SPLIT -------");
+        string[] splitPalabras = texto.Split(" ");
+        Console.WriteLine(splitPalabras);
 
+        Console.WriteLine("------- ENDWITH -------");
+        // StartsWith
+        Console.WriteLine("\nStartsWith:");
+        Console.WriteLine(texto.StartsWith("Programacion"));
+
+        // EndsWith
+        Console.WriteLine("\nEndsWith:");
+        Console.WriteLine(texto.EndsWith("divertida"));
+
+        // Contains
+        Console.WriteLine("\nContains:");
+        Console.WriteLine(texto.Contains("C#"));
+
+        // IndexOf
+        Console.WriteLine("\nIndexOf:");
+        Console.WriteLine(texto.IndexOf("en"));
+
+        // LastIndexOf
+        Console.WriteLine("\nLastIndexOf:");
+        Console.WriteLine(texto.LastIndexOf("a"));
+
+        // Insert
+        Console.WriteLine("\nInsert:");
+        string insertado = texto.Insert(13, "basica y ");
+        Console.WriteLine(insertado);
+
+        // Remove
+        Console.WriteLine("\nRemove:");
+        string removido = texto.Remove(13, 3);
+        Console.WriteLine(removido);
+
+        // Replace
+        Console.WriteLine("\nReplace:");
+        string reemplazado = texto.Replace("divertida", "interesante");
+        Console.WriteLine(reemplazado);
+    
     }
-}
+} 
