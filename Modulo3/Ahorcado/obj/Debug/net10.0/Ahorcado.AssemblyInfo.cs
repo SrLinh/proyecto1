70976be3524c2026a3d6535981c0247b707afa3f
SrLinh <mariostevjimenez@gmail.com>
2026-06-12 00:04:19 -0600

    Assemblyinfo changes

--- Modulo3/Ahorcado/obj/Debug/net10.0/Ahorcado.AssemblyInfo.cs
+++ Modulo3/Ahorcado/obj/Debug/net10.0/Ahorcado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ahorcado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03ee1a7a5fddecfc6402b2f518ba7c58e0e9d87e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eacb9b7449dcd9708968c7f6806d8c920395f294")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ahorcado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ahorcado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
