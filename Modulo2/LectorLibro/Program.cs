a996fc13afab4f10f6588108386d02119c86c212
SrLinh <mariostevjimenez@gmail.com>
2026-06-12 04:07:14 -0600

    Se agregó en Modulo2 un proyecto sobre leer un archivo .txt "LectorLibro"

---
+++ Modulo2/LectorLibro/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Libro libro = new Libro();
+        libro.test();
+    }
+}
+
+class Libro
+{
+    public void conteo()
+    {
+        string texto = File.ReadAllText("libro.txt");
+        string[] lineas = File.ReadAllLines("libro.txt");
+
+        int numLineas = lineas.Length;
+        Console.WriteLine($"La cantidad de lineas dentro del archivo son: {numLineas}");
+    }
+
+    public void test()
+    {
+        string frase = "Hola          a todos   ";
+        string[] palabras = frase.Split(' ');
+        foreach (string palabra in palabras)
+        {
+            Console.WriteLine(palabra);
+        }
+    }
+}
