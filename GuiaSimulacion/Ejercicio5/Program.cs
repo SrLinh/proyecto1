7b584edc5256bee5f2fe466f2aa7028760ab0166
SrLinh <mariostevjimenez@gmail.com>
2026-06-15 19:58:06 -0600

    Avance del Ejercicio5

--- GuiaSimulacion/Ejercicio5/Program.cs
+++ GuiaSimulacion/Ejercicio5/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main (string [] args)
+    {
+        
+    }
+}
+
+class Venta
+{
+    public string? vendedor {get; set;}
+    public double Monto {get; set;}
+
+    List<Venta> venta = new List<Venta>();
+
+    public void VentaArchivo()
+    {
+        if (File.Exists("ventas.txt"))
+        {
+            File.WriteAllText("ventas.txt","");
+        }
+        else
+        {
+            
+        }
+    }
+
+    public void Reader(){
+        StreamReader reader
+    }
+}
