10b7b511b8a62ce24415557281cf18ef993517b6
SrLinh <mariostevjimenez@gmail.com>
2026-06-24 16:27:21 +0200

    cambios sobre la prueba evaluada

---
+++ Linares_Mario_EvaluacionCSharp/Linares_Mario_Ejercicio6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+class Tarea
+{
+    public string? Titulo {get; set;}
+    public DateTime FechaLimite {get; set;}
+    public bool EsCompletada {get; set;}
+
+    public Tarea(string titulo, DateTime fechalimite, bool compleatda)
+    {
+        Titulo = titulo;
+        FechaLimite = fechalimite;
+        EsCompletada = compleatda;
+    }
+}
+
+class Sistema
+{
+    List<Tarea> tareas = new();
+    public static void Inicio()
+    {
+        if (!File.Exists("tareas.json"))
+        {
+            
+        }
+    }
+}
